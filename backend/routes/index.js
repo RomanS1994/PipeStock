8440c76a84f623c18d0c8ebd56e7b0e44225e629
Roman Stryzhka <140334590+RomanS1994@users.noreply.github.com>
2026-09-11 23:23:31 +0200

    cleanup: remove unused upload route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,13 +7,11 @@ import { handleOrderListRoutes } from './order-list.js';
 import { handleOrderRoutes } from './orders.js';
 import { handleProjectRoutes } from './projects.js';
 import { handlePublicRoutes } from './public.js';
-import { handleUploadRoutes } from './uploads.js';
 
 const routeHandlers = [
   handlePublicRoutes,
   handleAuthRoutes,
   handleMeRoutes,
-  handleUploadRoutes,
   handleProjectRoutes,
   handleManagerRoutes,
   handleMaterialRoutes,
